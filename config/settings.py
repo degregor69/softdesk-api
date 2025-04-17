1fe9d86beb5242a584eefd58b7f1638e75ec6a29
degregor69 <raphael.degregorio@gmail.com>
2025-04-17 21:24:04 +0200

    created users table

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "rest_framework_simplejwt",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import date
+
+
+def validate_age(value):
+    today = date.today()
+    age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+    if age < 15:
+        raise ValidationError("Vous devez avoir au moins 15 ans pour créer un compte.")
+
+class User(AbstractUser):
+    can_be_contacted = models.BooleanField(default=True)
+    can_data_be_shared = models.BooleanField(default=False)
+    birth_date = models.DateField(validators=[validate_age])
