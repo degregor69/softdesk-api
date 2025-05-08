17ef5b4c675637a8ea79a0b5cd1b6b336d7bc90b
degregor69 <raphael.degregorio@gmail.com>
2025-05-08 11:06:38 +0200

    added check of request_user is contributor when creating issue

--- issues/views.py
+++ issues/views.py
@@ -2,8 +2,21 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Issue
 from .serializers import IssueSerializer
+from rest_framework.exceptions import PermissionDenied
+from projects.models import Project
+from contributors.models import Contributor
+from django.shortcuts import get_object_or_404
 
 class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        project_id = self.request.data.get('project')
+        project = get_object_or_404(Project, pk=project_id)
+
+        if not Contributor.objects.filter(user=self.request.user, project=project).exists():
+            raise PermissionDenied("Vous devez être contributeur de ce projet pour créer une issue.")
+
+        serializer.save(created_by=self.request.user)
