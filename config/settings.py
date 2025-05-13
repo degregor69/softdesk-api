0db84b639b17b9069738060d3595b379dfb6565c
degregor69 <raphael.degregorio@gmail.com>
2025-05-13 13:38:16 +0200

    updated access token and refresh delays

--- config/settings.py
+++ config/settings.py
@@ -139,5 +139,5 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
 }
