00004da7d677222251c43f2aafb3d070174a0927
degregor69 <raphael.degregorio@gmail.com>
2025-05-20 09:02:09 +0200

    changed logic for creating issue from perform_create to create

--- issues/views.py
+++ issues/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Issue
 from .serializers import IssueSerializer
@@ -12,12 +13,15 @@ class IssueViewSet(viewsets.ModelViewSet):
     serializer_class = IssueSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        project_id = self.request.data.get('project')
+    def create(self, request, *args, **kwargs):
+        project_id = request.data.get('project')
         project = get_object_or_404(Project, pk=project_id)
-        contributor = Contributor.objects.filter(user=self.request.user, project=project).first()
+        contributor = Contributor.objects.filter(user=request.user, project=project).first()
 
         if not contributor:
             raise PermissionDenied("Vous devez être contributeur de ce projet pour créer une issue.")
 
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save(created_by=contributor)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
