d26999f4d1dc199074cd70dea7b567aa948b9162
degregor69 <raphael.degregorio@gmail.com>
2025-06-07 13:13:59 +0200

    removed hello_world in api urls

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
 
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({"message": "Hello, SoftDesk API!"})
 
 
 urlpatterns = [
-    path('', hello_world),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
